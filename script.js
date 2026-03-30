5a740a190a0df82be93cff59b6fef7e7f3d00980
renatodyas01-prog <renatodyas01@gmail.com>
2026-03-30 10:05:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+let total = 0;
+
+document.querySelectorAll("input").forEach(e=>{
+  e.addEventListener("change", ()=>{
+    total = 0;
+
+    document.querySelectorAll("input:checked").forEach(c=>{
+      total += parseInt(c.value);
+    });
+
+    document.getElementById("total").innerText = total;
+  });
+});
+
+function enviarWhats(){
+
+  let msg = `Olá, fiz um orçamento:
+
+Total: R$ ${total}
+
+Meu problema é:`;
+
+  window.open(`https://wa.me/5549999570660?text=${encodeURIComponent(msg)}`);
+}
