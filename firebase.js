5fb328e36767a5606c5967e9bef58121db20d9a4
renatodyas01-prog <renatodyas01@gmail.com>
2026-03-30 10:06:26 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
